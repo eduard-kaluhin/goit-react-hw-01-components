ee785abbcc88d3918d5a43e9007c8b83d12c7c8c
eduard-kaluhin <renoserviser@gmail.com>
2023-07-17 13:36:27 +0300

    Create FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { List } from './FriendList.styled';
+import { FriendListItem } from './FriendListItem/FriendListItem';
+
+export default function FriendList({ friends }) {
+  return (
+    <List className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </List>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
